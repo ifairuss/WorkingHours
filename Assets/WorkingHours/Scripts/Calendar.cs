9359a4bcd3d2472473355d4b5a305e8bafcb9af8
Artem Voloshyn <147313250+ifairuss@users.noreply.github.com>
2026-05-09 17:47:41 +0200

    Reapply "Feature/Calendar/SCHEDULE"
    This reverts commit e0b394dd26db6896dfbccd311bc5a9dbccd5e5bd.

--- Assets/WorkingHours/Scripts/Calendar.cs
+++ Assets/WorkingHours/Scripts/Calendar.cs
@@ -1,15 +1,21 @@
 using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Calendar : MonoBehaviour
 {
     public static Calendar Instance { get; private set; }
 
-    [SerializeField] private float TimeHour;
-    [SerializeField] private float TimeMinute;
-    [SerializeField] private int Day;
-    [SerializeField] private int Month;
-    [SerializeField] private int Year;
+    [SerializeField] private List<GameObject> _allDayInMonth;
+
+    [SerializeField] private TextMeshProUGUI _dateText;
+    [SerializeField] private TextMeshProUGUI _timeText;
+
+    private string Day;
+    private string Month;
+    private string Year;
+    private int DayInMounth;
 
     private void Awake()
     {
@@ -18,14 +24,32 @@ public class Calendar : MonoBehaviour
 
     public void Initialized()
     {
-        Year = DateTime.Now.Year;
-        Month = DateTime.Now.Month;
-        Day = DateTime.Now.Day;
+        DisableAllDay();
+
+        Day = DateTime.Now.Day.ToString().PadLeft(2, '0');
+        Month = DateTime.Now.Month.ToString().PadLeft(2, '0');
+        Year = DateTime.Now.Year.ToString();
+
+        _dateText.text = $"{Day}/{Month}/{Year}";
+
+        DayInMounth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+
+        for (int i = 0; i < DayInMounth; i++)
+        {
+            _allDayInMonth[i].SetActive(true);
+        }
+    }
+
+    private void DisableAllDay()
+    {
+        for (int i = 0; i < _allDayInMonth.Count; i++)
+        {
+            _allDayInMonth[i].SetActive(false);
+        }
     }
 
     private void Update()
     {
-        TimeHour = DateTime.Now.Hour;
-        TimeMinute = DateTime.Now.Minute;
+        _timeText.text = $"{DateTime.Now.Hour}:{DateTime.Now.Minute.ToString().PadLeft(2,'0')}";
     }
 }
