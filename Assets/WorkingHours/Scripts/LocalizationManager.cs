e52a43d1676eab8edf2232b67b4ea4cfd9d92605
Artem Voloshyn <147313250+ifairuss@users.noreply.github.com>
2026-05-25 16:10:37 +0200

    Feature/Localization

---
+++ Assets/WorkingHours/Scripts/LocalizationManager.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> _allWords;
+}
